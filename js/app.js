fd7b70574da7c4c83e261c0b2fbba12a0c5e8dbe
Kainã Moreira <78170911+KMoreira@users.noreply.github.com>
2024-01-31 13:45:41 +0000

    Desafio Completo

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
+function alterarStatus(id){
+    let indexGame = document.getElementById(`game-${id}`);
+    let imagem = indexGame.querySelector('.dashboard__item__img');
+    let botao = indexGame.querySelector('.dashboard__item__button');
 
-let paragrafo = document.querySelector('a');
-let index = [1, 2, 3];
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    }else{
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 
-
-function alterarStatus(id) {
-    let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let button = gameClicado.querySelector('.dashboard_item_button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-
-    alert(nomeJogo.textContent);    
 }
-
